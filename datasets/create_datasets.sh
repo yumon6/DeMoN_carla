71a1d69883062b7d5e86ed3d3c90fae2d36faa20
yumon6 <50375564+yumon6@users.noreply.github.com>
2019-08-27 02:16:31 +0900

    Create create_datasets.sh

---
+++ datasets/create_datasets.sh
@@ -0,0 +1,12 @@
+python create_rt.py
+print('==============================')
+print('txt creation completed')
+print('==============================')
+python create_depth.py
+print('==============================')
+print('npy creation completed')
+print('==============================')
+python create_datasets.py
+print('==============================')
+print('datasets creation completed')
+print('==============================')
