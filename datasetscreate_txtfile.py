d6ddc60974ec10f4332c7a12df72594622d8154d
yumon6 <50375564+yumon6@users.noreply.github.com>
2019-08-26 21:00:35 +0900

    Create datasetscreate_txtfile.py

---
+++ datasetscreate_txtfile.py
@@ -0,0 +1,11 @@
+import csv
+
+with open('./image/measurements.csv') as f:
+    reader = csv.reader(f)
+    l = [row for row in reader]
+
+
+for i in range(10000)
+    f = open("" + ".txt", "w")
+    f.write("r11 r12 r13 t1\nr21 r22 r23 t2\nr31 r32 r33 t3")
+    f.close()
