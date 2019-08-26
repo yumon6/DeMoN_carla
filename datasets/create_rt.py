1cbec38ad72167e4eb8e4121981563643748b7e3
yumon6 <50375564+yumon6@users.noreply.github.com>
2019-08-26 21:09:41 +0900

    Update create_rt.py

--- datasets/create_rt.py
+++ datasets/create_rt.py
@@ -5,6 +5,6 @@ with open('./image/measurements.csv') as f:
     l = [row for row in reader]
     
 for i in range(10000)
-    f = open("rt.txt", "w")
+    f = open("./image/rt" + "rt.txt", "w")
     f.write("r11 r12 r13 t1\nr21 r22 r23 t2\nr31 r32 r33 t3")
     f.close()
