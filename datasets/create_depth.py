98ab5e404cd84bff7d2c30cd0dbd28a8bcf730d7
yumon6 <50375564+yumon6@users.noreply.github.com>
2019-08-26 20:37:57 +0900

    Update create_depth.py

--- datasets/create_depth.py
+++ datasets/create_depth.py
@@ -3,6 +3,6 @@ import numpy as np
 from matplotlib import pylab as plt
 
 for i in range(10000)
-    img = np.array(Image.open((str(i+2000)+'.jpg').convert('L'), 'float32')
-    np.save('str(i+2000).npy',img)
+    img = np.array(Image.open('./image/rgb' + str(i+2000)+'.jpg').convert('L'), 'float32')
+    np.save('./image/depth' + 'str(i+2000).npy',img)
     print('convert depth image str(i+2000)')
